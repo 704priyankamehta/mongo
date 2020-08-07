1d9e8b6f604a9480c90eddae70fe0c06dab5e40c
704priyankamehta <priyanka.mehta@kelltontech.com>
2020-08-07 11:33:16 +0530

    first

--- modules/data.js
+++ modules/data.js
@@ -11,108 +11,56 @@ function checkSession(req, res, next) {
     //return res.status(401).send("login first");
     //}
     //res.status(200).send(req.session.user)
-    const token = req.headers.authorization.split(" ")[1];
-    try{
-    console.log(token);
 
-    var decode=jwt.verify(token,'shhhhh');
-    console.log(decode._doc);
-    next();
-}catch(err){
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+
+        var decode = jwt.verify(token, 'shhhhh');
+        console.log(decode._doc);
+        next();
+    } catch (err) {
         res.status(401).send("invalid token");
     }
 }
 
-async function checkuserExit(userId, user) {
-    const result = await registration.findById(userId, function (err) {
-        if (err) {
-            res.send(404);
-        }
-    });
-    if (user.email == result.email) {
-        var userExit = 1;
-        return userExit;
-    }
-    else {
-        var userExit = -1;
-        return userExit;
-    }
-}
 
-<<<<<<< HEAD
 async function get(req, res) {
-   
+
     var email = req.body.email;
     var password = req.body.password;
-    if(!email || !password){
-        return res.send("required email or password");
-     } 
-   
     const user = await registration.findOne({ email: email })
     if (!user) {
-        res.send(404,"no user found");
+        res.send(404, "no user found");
     }
-    bcrypt.compare(password, user.password, function (err,result) {
+    bcrypt.compare(password, user.password, function (err, result) {
         if (err) {
-            res.send("incmplete data");
+            res.send(404, "incmplete data");
         }
         if (result == false) {
             return res.send("invalid email or id");
 
         }
         else {
-            var token = jwt.sign({ ...user, _id: user._id.toString() }, 'shhhhh');
-            //req.session.user = user;
-            
-            return res.status(200).json({message:"logged in sucessfully",token:token});
+            var token = jwt.sign({ ...user, _id: user._id.toString() }, 'shhhhh',{expiresIn:'600'});
+            return res.status(200).json({ message: "logged in sucessfully", token: token });
 
         }
     })
 
 }
 async function getdata(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
-    if(!email || !password){
-       return res.send("required email or password");
-    } 
-
-    const user = await registration.findOne({ email: email })
-    if (!user) {
-        res.send(404,"no user found");
+    let user = req.body;
+    const userId = req.params.id;
+    const response = await registration.findById(userId, user);
+    if (!response) {
+        res.send(404, "user not exit");
     }
-    bcrypt.compare(password, user.password).then(function (result) {
-        if (result == true) {
-            //req.session.user = user;
-            
-            return res.status(200).send(user);
-        }
-        else {
-            return res.send("invalid email or id");
-        }
-    }).catch(err=>res.send("incomplete data"));
-=======
-
-function getdata(req,res){
-    var userId=req.body.userId;
-    var password=req.body.password;
-  
-    registration.findOne({userId:userId,password:password},function(err,user){
-      if(err){
-        return res.send(err);
-  
-      }
-      if(!user){
-        return res.send(404,{message:"invalid user id or password"})
-      }
-      req.session.user=user;
-      return res.status(200).send();
-    })};
-
->>>>>>> origin/master
-
+    //req.session.user = response;
+    res.send(200);
 
 }
+
 const createdata = async function (req, res, next) {
     var name = req.body.name;
     var email = req.body.email;
@@ -135,26 +83,16 @@ const createdata = async function (req, res, next) {
 const updatedata = async function (req, res, next) {
     let user = req.body;
     const userId = req.params.id;
-    var userExit = await checkuserExit(userId, user);
-    console.log(userExit);
-    if (userExit == -1) {
-        return res.send("user not exit");
-    }
     const response = await registration.findByIdAndUpdate(userId, user);
+    if (!response) {
+        res.send(404, "user not exit");
+    }
     //req.session.user = response;
     res.send(200);
-
 }
-
 const deletedata = async function (req, res, next) {
     const userId = req.params.id;
-    let user = req.body;
-    var userExit = await checkuserExit(userId, user);
-    console.log(userExit);
-    if (userExit == -1) {
-        return res.send("user not exit");
-    }
-    await registration.findByIdAndRemove(userId)
+    var user = await registration.findByIdAndRemove(userId)
     //req.session.user = response;
     res.send(200, { message: "deleted" });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var data = require("../modules/data");
 var router = express.Router();
-
-
-
-
-router.post('/login' ,data.get);
+function validation(req,res,next){
+    var email = req.body.email;
+    var password = req.body.password;
+    if(!email || !password){
+        return res.send("required email or password");
+     }
+      next();
+}
+router.post('/login' ,validation,data.get);
 router.post('/registration',data.createdata);
-router.get('/login',data.checkSession ,data.getdata);
+router.get('/login/:id',data.checkSession ,data.getdata);
 router.put('/registration/:id',data.checkSession , data.updatedata);
-
 router.delete('/registration/:id',data.checkSession , data.deletedata);
 
 
